b065110e3238ef1f93c6689eb60b721272f2ceff
Don-Claudio <claudiodex009@gmail.com>
2026-07-26 19:27:54 +0300

    feat: add fifo_if with clocking block for TB-side signal timing

---
+++ tb/fifo_if.sv
@@ -0,0 +1,18 @@
+interface fifo_if #(parameter WIDTH = 16) (input logic clk);
+
+  logic             reset;
+  logic             w_enb, r_enb;
+  logic [WIDTH-1:0] din;
+  logic [WIDTH-1:0] dout;
+  logic             full, empty;
+
+  clocking cb @(posedge clk);
+    output w_enb, r_enb;
+    output din;
+    input dout;
+    input full, empty;
+  endclocking : cb
+
+  modport TB (clocking cb);
+
+endinterface : fifo_if
